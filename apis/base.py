6956c8a74cd6d40d5fae2f7ec251ddd2bc4c1a5d
janrossnicoloo <janrossnicolo_olano@trendmicro.com>
2022-05-02 19:29:18 +0800

    Update config

--- apis/base.py
+++ apis/base.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(process_image.router, prefix="/image", tags=["users"])
+api_router.include_router(process_image.router, prefix="/image", tags=["image"])
--- core/config.py
+++ core/config.py
@@ -8,24 +8,6 @@ load_dotenv(dotenv_path=env_path)
 
 
 class Settings:
-    PROJECT_NAME: str = "Job Board"
-    PROJECT_VERSION: str = "1.0.0"
-
-    USE_SQLITE_DB: str = os.getenv("USE_SQLITE_DB")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv(
-        "POSTGRES_PORT", 5432
-    )  # default postgres port is 5432
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30  # in mins
-
-    TEST_USER_EMAIL = "test@example.com"
     PEXEL_API_KEY = os.getenv("PEXEL_API_KEY")
 
     CLOUDINARY_NAME = os.getenv("CLOUDINARY_NAME")
@@ -33,5 +15,7 @@ class Settings:
     CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
     CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
 
+    MONGO_DB_SERVER = os.getenv("MONGO_DB_SERVER")
+    MONGO_DB_NAME = os.getenv("MONGO_DB_SERVER")
 
 settings = Settings()
